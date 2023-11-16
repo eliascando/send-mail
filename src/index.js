a1397b36f31e70d9cb6362bca6c56e56c210a298
Elias Cando <elias.candoj@gmail.com>
2023-11-16 01:44:20 -0500

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { PORT } = require('./../config');
 const sendEmail = require('./controllers/mail.controller');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
