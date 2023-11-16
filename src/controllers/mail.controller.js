f663f9891f22fea33cb4e9c429d0358bcd5a3721
Elías Cando <75767835+eliascando@users.noreply.github.com>
2023-11-16 14:28:18 -0500

    Update mail.controller.js

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -2,6 +2,15 @@ const sendMail = require('./../services/mail.services');
 
 const sendEmail = async(req, res) => {
     const { name, email, subject, message } = req.body;
+
+    if (!name || !email || !suject || !message){
+        res.status(400).json({
+            status: 'error',
+            message: 'Bad request'
+        });
+        return;
+    }
+    
     let sended = await sendMail(name, email, subject, message);
 
     if(!sended){
